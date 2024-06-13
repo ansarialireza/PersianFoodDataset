0c6ae3e71a5961fa0c270f6c8e5ba3d72bc2d35e
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-06-13 16:34:38 +0330

    update template and dataset model

--- dataset/models.py
+++ dataset/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='food_images/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
