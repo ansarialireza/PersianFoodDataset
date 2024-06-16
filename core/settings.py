83601d8379693cae6486a90acebfea0287546ae0
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-06-16 15:39:52 +0330

    update

--- core/settings.py
+++ core/settings.py
@@ -10,11 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-sutsg(e3xrin8bjz7par8)(zsmqs+x$ts^0f0t$u_5mm5eoz0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
-ALLOWED_HOSTS = config("ALLOWED_HOSTS",default=False,cast=tuple)
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from django.views.generic import TemplateView
 from dataset.models import Category
+from django.conf import settings
+
 class Index(TemplateView):
-    template_name='website/index.html'
+    template_name = 'website/index.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
+        context['site_key'] = settings.RECAPTCHA_PUBLIC_KEY
         return context
