02edca25ec12f5cc91ec345d5e8d8ebc96488a17
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-06-27 21:28:21 +0330

    update

--- dataset/models.py
+++ dataset/models.py
@@ -1,3 +1,4 @@
+# models.py
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from .utils import food_image_path 
--- dataset/utils.py
+++ dataset/utils.py
@@ -1,6 +1,8 @@
+# utils.py
+
 import os
 from django.db.models import Max
-from django.conf import settings
+from django.utils.text import get_valid_filename
 
 def get_latest_id():
     from dataset.models import FoodImage
@@ -8,13 +10,17 @@ def get_latest_id():
     return max_id if max_id is not None else 0
 
 def generate_filename(instance, filename):
-    category_name = instance.category.name
     category_id = instance.category.id 
     rating = instance.rating
     unique_id = get_latest_id() + 1
     
-    new_filename = f"{category_id}_{rating}_{unique_id}.jpg"  
-    return os.path.join(settings.MEDIA_ROOT, 'food_images', str(category_id), new_filename)
+    clean_filename = f"{category_id}_{rating}_{unique_id}.jpg"
+    safe_filename = get_valid_filename(clean_filename)
+    
+    # Constructing the relative path
+    final_path = os.path.join('food_images', safe_filename)
+    
+    return final_path
 
 def food_image_path(instance, filename):
     return generate_filename(instance, filename)
