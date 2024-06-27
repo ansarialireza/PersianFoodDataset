a5b1de020e861c9af8d2ebdbac686b7cc95b00ef
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-06-27 15:33:59 +0330

    update

--- dataset/utils.py
+++ dataset/utils.py
@@ -1,36 +1,19 @@
 import os
-import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
-from dataset.models import FoodImage
 from django.db.models import Max
+from django.conf import settings
 
-def get_max_food_image_id():
-    # کوئری برای یافتن بیشترین شناسه (id) FoodImage
+def get_latest_id():
+    from dataset.models import FoodImage
     max_id = FoodImage.objects.aggregate(max_id=Max('id'))['max_id']
+    return max_id if max_id is not None else 0
+
+def generate_filename(instance, filename):
+    category_name = instance.category.name
+    rating = instance.rating
+    unique_id = get_latest_id() + 1
     
-    # اگر رکوردی وجود داشت، بیشترین شناسه را برگردانید
-    if max_id is not None:
-        return max_id
-    else:
-        # اگر هیچ رکوردی پیدا نشد، عدد صفر را برگردانید
-        return 0
+    new_filename = f"{rating}_{unique_id}.jpg"
+    return os.path.join(settings.MEDIA_ROOT, 'food_images', category_name, new_filename)
 
 def food_image_path(instance, filename):
-    # instance is the instance of FoodImage being saved.
-    # filename is the original filename of the uploaded file.
-    
-    # Get category name
-    category_name = instance.category.name
-    
-    # Get the rating
-    rating = instance.rating
-    
-    # Generate a unique numerical identifier
-    unique_id = get_max_food_image_id()+1
-    
-    # Generate new filename
-    new_filename = f"{rating}_{unique_id}.jpg"
-    
-    # Return the path where the file will be uploaded
-    return os.path.join('food_images', category_name, new_filename)
+    return generate_filename(instance, filename)
