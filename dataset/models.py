717a0e3d710037121e6c94bee25feab57a3798d1
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-06-29 17:17:51 +0330

    final update

--- dataset/models.py
+++ dataset/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from .utils import food_image_path 
+from django.core.exceptions import ValidationError
 
 class Uploader(models.Model):
     name = models.CharField(max_length=100)
@@ -17,17 +18,26 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+def validate_image_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
+
 class FoodImage(models.Model):
     uploader = models.ForeignKey(Uploader, on_delete=models.CASCADE, related_name='foods')
-    image = models.ImageField(upload_to=food_image_path)
+    image = models.ImageField(upload_to=food_image_path, validators=[validate_image_file_extension])
     upload_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='foods')
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
+
     def __str__(self):
         return f"Uploader: {self.uploader.name}, Category: {self.category.name}"
-
 
 class Question(models.Model):
     text = models.CharField(max_length=255)
